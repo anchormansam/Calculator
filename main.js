3ec4f1dda97f0eb3f5849270e9fabb9f55f5778c
Sam Wheeling <anchormansam@gmail.com>
2019-09-30 17:00:37 -0400

    updated logic

--- main.js
+++ main.js
@@ -1,12 +1,77 @@
+var equalPressed = false;
 var A = document.getElementById('Calculator');
 A.setAttribute('class', 'container text-center');
 var keys = ['C', '', '', '/', '7', '8', '9', '*', '4', '5', '6', '-', '1', '2', '3', '+', '0', '', '.', '='];
 var input = [];
+var total = 0;
+
 
 // var defaultClear needs to clear after = is hit and another number  if operator has not been hit before a number
 
 var title = document.createElement('div');
 title.innerHTML = 'DO YOU MATH?';
+
+function calculate() {
+    // console.log(input);
+    switch (this.innerHTML) {
+        case '+':
+        case '-':
+        case '/':
+        case '*':
+            if(equalPressed){
+                // console.log('in equals pressed == false',this.innerHTML);
+                input = [];
+                // displayCharacters(0);
+            }
+                // numbers(this.innerHTML);
+                operators(this.innerHTML);
+            
+            break;
+        case '=':
+            equalPressed = true;
+            var firstValue = parseFloat(input[0]);
+            var operator = input[1];
+            var secondValue = parseFloat(input[2]);
+            var total = 0;
+            if (operator) {
+                switch (operator) {
+                    case '+': total = firstValue + secondValue;
+                        break;
+                    case '-': total = firstValue - secondValue;
+                        break;
+                    case '/': total = firstValue / secondValue;
+                        break;
+                    case '*': total = firstValue * secondValue;
+                        break;
+                }
+                displayCharacters(total);
+                input = [total.toString()];
+                // console.log(input);
+            }
+            // if a number is entered after = is hit then clear total 
+            break;
+
+        case '.':
+            decimal();
+            numbers(this.innerHTML);
+            break;
+        case 'C':
+            input = [];
+            displayCharacters(0);
+            break;
+        default:
+            if (equalPressed == true) {
+                equalPressed = false;
+                input = [];
+                displayCharacters(0);
+            }
+            numbers(this.innerHTML);
+            
+    }
+    console.log(input);
+}
+
+// console.log(input);
 
 
 function createCalc() {
@@ -20,12 +85,9 @@ function createCalc() {
     display.setAttribute('id', 'output');
     display.innerHTML = "0";
 
-
-
     A.appendChild(calculator);
     A.appendChild(title);
     A.appendChild(display);
-
 
     var newRow = document.createElement('div');
     newRow.setAttribute('class', 'row d-flex align-items-stretch ');
@@ -35,61 +97,11 @@ function createCalc() {
         newCol.setAttribute('class', 'col-3 button item-self-center border bg-light' + c);
         newCol.setAttribute('type', 'button');
         newCol.setAttribute('id', 'col' + c);
-
         newCol.innerHTML = `${keys[c]}`;
 
         if (keys[c]) {
-            newCol.addEventListener("click", function () {
-                // console.log(input);
-
-                switch (this.innerHTML) {
-                    case '+':
-                    case '-':
-                    case '/':
-                    case '*':
-                        operators(this.innerHTML);
-                        break;
-                    case '=':
-                        var firstValue = parseFloat(input[0]);
-                        var operator = input[1];
-                        var secondValue = parseFloat(input[2]);
-                        var total = 0;
-                        if (operator) {
-                            switch (operator) {
-                                case '+': total = firstValue + secondValue;
-                                    break;
-                                case '-': total = firstValue - secondValue;
-                                    break;
-                                case '/': total = firstValue / secondValue;
-                                    break;
-                                case '*': total = firstValue * secondValue;
-                                    break;
-                            }
-                            displayCharacters(total);
-                            input = [total.toString()];
-                        }
-                        // if a number is entered after = is hit then clear total 
-                        break;
-
-                    case '.':
-                        decimal();
-                        numbers(this.innerHTML);
-                        break;
-                    case 'C':
-                        input = [];
-                        displayCharacters(0);
-                        break;
-                    default:
-                        numbers(this.innerHTML);
-
-                }
-
-
-            });
+            newCol.addEventListener("click", calculate);
         }
-
-
-
         newRow.appendChild(newCol);
         calculator.appendChild(newRow);
     } A.appendChild(calculator);
@@ -98,16 +110,15 @@ function createCalc() {
 // Display Key Characters
 function displayCharacters(displayCharacter) {
     // console.log('displayCharacter:', displayCharacter);
-
     var displayCalc = document.getElementById('output');
-
     displayCalc.innerHTML = displayCharacter;
 }
 
 function numbers(n) {
+    // console.log(input);
     var length = input.length;
     // Check string length for x digits
-
+    // console.log(input);
     if (length > 0) {
         // we have a number that needs to be added
         if (parseInt(input[length - 1], 10) >= 0) {  // 0. == 0
@@ -116,13 +127,14 @@ function numbers(n) {
                 input[length - 1] = input[length - 1] + n;
             }
 
-            console.log(length, input)
+            // console.log(length, input)
         }
 
         else {
             // we need to add an operator or a decimal
-            console.log(input);
+            // console.log(input);
             input.push(n);
+           
 
         }
     }
@@ -134,22 +146,15 @@ function numbers(n) {
 
 
 function operators(o) {
+    console.log(o);
     var length = input.length;
-    // console.log(input)
+    // var retain = ;
+    // console.log(retain)
     if (length > 0) {
         input[1] = o;
-        // if (['+', '-', '*', '/'].includes(input[1])) {
-        //     console.log('this work?')
-        //     input[] = o;
-        // }
-        // else {
-        //     console.log('this work as well?')
-        // }
     }
-    // else {
-    //     input.push(o);
-    // }
-    // displayCharacters(input[input.length - 1]);
+    console.log();
+
 }
 
 
